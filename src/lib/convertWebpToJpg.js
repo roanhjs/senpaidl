d633de16ad8a9c362bb4f170403eb8f1faee97da
roanhjs <roanhjs@gmail.com>
2025-04-09 11:17:56 -0400

    Remove deprecated senpaidl service code

--- src/lib/convertWebpToJpg.js
+++
@@ -1,7 +0,0 @@
-import sharp from "sharp";
-
-export async function convertWebpToJpg(buffer) {
-  const convertWebpToJpg = sharp(buffer).toFormat("jpeg");
-  const toBuffer = await convertWebpToJpg.toBuffer();
-  return toBuffer;
-}
